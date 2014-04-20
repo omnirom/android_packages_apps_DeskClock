92752ae57abcaec363ab79ea9a666b323705c3b7
Felix Ableitner <me@nutomic.com>
2014-04-20 14:12:05 +0200

    Fixed days of week in alarm clock settings not respecting current locale.
    Change-Id: I90d3f605672844a517353e264e8368e3c456f86b

--- src/com/android/deskclock/AlarmClockFragment.java
+++ src/com/android/deskclock/AlarmClockFragment.java
@@ -688,15 +688,21 @@ public class AlarmClockFragment extends DeskClockFragment implements
         private final int mCollapseExpandHeight;
 
         // This determines the order in which it is shown and processed in the UI.
-        private final int[] DAY_ORDER = new int[] {
-                Calendar.SUNDAY,
-                Calendar.MONDAY,
-                Calendar.TUESDAY,
-                Calendar.WEDNESDAY,
-                Calendar.THURSDAY,
-                Calendar.FRIDAY,
-                Calendar.SATURDAY,
-        };
+        private final int[] DAY_ORDER = getDayOrder();
+
+        /**
+         * Generates day order according to Calendar.getFirstDayOfWeek() in the current locale.
+         */
+        private int[] getDayOrder() {
+            int[] order = new int[7];
+            Calendar cal = Calendar.getInstance();
+            cal.set(Calendar.DAY_OF_WEEK, cal.getFirstDayOfWeek());
+            for (int i = 0; i < 7; i++) {
+                order[i] = cal.get(Calendar.DAY_OF_WEEK);
+                cal.roll(Calendar.DAY_OF_WEEK, 1);
+            }
+            return order;
+        }
 
         public class ItemHolder {
 
